d81a33b18f9b0ac6c07e2dc2b4564623dd5f79f5
Sprvn <kevin.gustafsson@gmail.com>
2016-03-22 10:22:45 +0100

    Added check against blacklist-variable

--- SpotifyTrackIgnore.sh
+++ SpotifyTrackIgnore.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+blacklist="Kung För En Dag - Magnus Uggla";
+
 result=`dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'`;
 
 song=`printf "$result\n" | grep -A2 -E "string \"xesam:title\"" | tail -n +2 | sed 's/            variant                string //g' | grep -E -o "\".*\"" | sed 's/\"//g'`;
 artist=`printf "$result\n" | grep -A2 -E "string \"xesam:artist\"" | tail -n +3 | sed 's/            variant                string //g' | grep -E -o "\".*\"" | sed 's/\"//g' | sed 's/                  string //g'`;
 
-echo "$song - $artist";
 
+both=`echo "$song - $artist"`;
 
+match=`echo $blacklist | grep "$both"`;
+
+if [ -n "$match" ]; then
+	echo "$match";
+	dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
+fi
