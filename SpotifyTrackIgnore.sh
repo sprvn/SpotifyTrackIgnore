3d1a7f58b192bd495e32bf5ba636fd7723f18c61
Sprvn <kevin.gustafsson@gmail.com>
2016-03-22 16:44:27 +0100

    Unset  before check against blacklist

--- SpotifyTrackIgnore.sh
+++ SpotifyTrackIgnore.sh
@@ -10,15 +10,15 @@ do
 	song=`printf "$result\n" | tr '[:upper:]' '[:lower:]' | grep -A2 -E "string \"xesam:title\"" | tail -n +2 | sed 's/            variant                string //g' | grep -E -o "\".*\"" | sed 's/\"//g'`;
 	artist=`printf "$result\n" | tr '[:upper:]' '[:lower:]' | grep -A2 -E "string \"xesam:artist\"" | tail -n +3 | sed 's/            variant                string //g' | grep -E -o "\".*\"" | sed 's/\"//g' | sed 's/                  string //g'`;
 
-	echo $song;
 	both=`echo "$song - $artist"`;
 
 	blacklist=`echo $blacklist | tr '[:upper:]' '[:lower:]'`;
-
+	match="";
 	match=`echo $blacklist | grep "$both"`;
+	echo "mat: $match";
 	if [ -n "$match" ]; then
 		echo "$match";
 		dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
-		#unset $match;
 	fi
+	sleep 1;
 done
