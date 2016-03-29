42f3b0569e80722d9fe57fe3c83e1cd988a019f3
Sprvn <kevin.gustafsson@gmail.com>
2016-03-29 11:38:25 +0200

    Added loop until next song comes on & read blacklist from file

--- SpotifyTrackIgnore.sh
+++ SpotifyTrackIgnore.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-blacklist="Kung För En Dag - Magnus Uggla";
-
 while :
 do
-
 	result=`dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'`;
 
 	song=`printf "$result\n" | tr '[:upper:]' '[:lower:]' | grep -A2 -E "string \"xesam:title\"" | tail -n +2 | sed 's/            variant                string //g' | grep -E -o "\".*\"" | sed 's/\"//g'`;
 	artist=`printf "$result\n" | tr '[:upper:]' '[:lower:]' | grep -A2 -E "string \"xesam:artist\"" | tail -n +3 | sed 's/            variant                string //g' | grep -E -o "\".*\"" | sed 's/\"//g' | sed 's/                  string //g'`;
 
-	both=`echo "$song - $artist"`;
+	both=`echo "$song - $artist" | sed 's/ //g'`;
 
-	blacklist=`echo $blacklist | tr '[:upper:]' '[:lower:]'`;
-	match="";
-	match=`echo $blacklist | grep "$both"`;
-	if [ -n "$match" ]; then
+        blacklistRes=`cat blacklist.dat | sed 's/ //g' | grep -oiE "$both"`
+
+#	blacklist=`echo $blacklist | tr '[:upper:]' '[:lower:]'`;
+#	match="";
+#	match=`echo $blacklist | grep "$both"`;
+	if [ -n "$blacklistRes" ]; then
+                echo $blacklistRes;
 		dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next
+
+                resultTemp="1";
+                while [ -n "$resultTemp" ]; do 
+                    echo "yoyoy"
+                    resultTemp=`dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'`;
+                    resultTemp=`echo $resultTemp | grep -oiE "$song"`;
+                done 
 	fi
 	sleep 0.25;
 done
